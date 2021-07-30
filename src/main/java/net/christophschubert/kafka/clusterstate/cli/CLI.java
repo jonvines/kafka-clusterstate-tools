7c5c1620715971159c23e61b7b97164b71679281
Jon Vines <jon.vines@ao.com>
2021-07-30 21:23:10 +0100

    Include the apply to cluster from the cloudgovernance class to the main cli class

--- src/main/java/net/christophschubert/kafka/clusterstate/cli/CLI.java
+++ src/main/java/net/christophschubert/kafka/clusterstate/cli/CLI.java
@@ -1,9 +1,14 @@
 package net.christophschubert.kafka.clusterstate.cli;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import net.christophschubert.kafka.clusterstate.ClientBundle;
 import net.christophschubert.kafka.clusterstate.ClusterState;
 import net.christophschubert.kafka.clusterstate.ClusterStateManager;
+import net.christophschubert.kafka.clusterstate.formats.env.CloudCluster;
+import net.christophschubert.kafka.clusterstate.formats.env.Environment;
+import net.christophschubert.kafka.clusterstate.utils.MapTools;
+import org.apache.commons.text.StringSubstitutor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import picocli.CommandLine;
@@ -12,11 +17,10 @@ import picocli.CommandLine.Option;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 import java.util.concurrent.ExecutionException;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 
 @Command(name= "kcs", subcommands = { CommandLine.HelpCommand.class }, version = "kcs 0.1.0",
@@ -57,6 +61,25 @@ class CLI {
         return 0;
     }
 
+    @CommandLine.Command(name = "applyToSelectedCluster", description = "Apply domain description from context to the selected cluster.")
+    int applyToSelectedCluster(
+            @CommandLine.Parameters(paramLabel = "environment", description = "path to the environment", defaultValue = ".") File contextPath,
+            @CommandLine.Parameters(paramLabel = "clustername", description = "the name of the cluster to work on") String clusterName
+    ) {
+        final ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        try {
+
+            final var environment = mapper.readValue(contextPath, Environment.class);
+
+            environment.clusters.stream().filter( c -> c.name.equals( clusterName ) ).forEach( c -> applyCluster(c, System.getenv()) );
+
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return 1;
+        }
+        return 0;
+    }
 
     @Command(name = "extract", description = "Extract the current state of the cluster")
     void extract(
@@ -80,7 +103,59 @@ class CLI {
         final ObjectMapper mapper = new ObjectMapper();
         mapper.writer().writeValue(stateFile, clusterState);
     }
+    /**
+     * The following entries from the envVars-map will used for substitutions:
+     *  KST_CLUSTER_API_SECRET_<clusterName>
+     *  KST_CLUSTER_API_KEY_<clusterName>
+     *  KST_SR_API_SECRET_<clusterName>
+     *  KST_SR_API_KEY_<clusterName>
+     *
+     * @param cluster
+     * @param envVars
+     */
+    void applyCluster(CloudCluster cluster, Map<String, String> envVars) {
 
+        final var substitutions = EnvVarTools.extractEnvVarsForCluster(cluster.name, envVars);
+
+        /*
+        env vars to create to
+         export KST_CLUSTER_API_SECRET_cluster_AO_QA=Q-api-secret
+         export KST_CLUSTER_API_KEY_cluster_AO_QA=Q-api-key
+         export KST_SR_API_SECRET_cluster_AO_QA=Q-sr-secret
+         export KST_SR_API_KEY_cluster_AO_QA=Q-sr-key
+         */
+
+        final StringSubstitutor substitutor = new StringSubstitutor(substitutions);
+
+        final var clientProps = new Properties();
+        clientProps.putAll(MapTools.mapValues(CLITools.getClientProps(cluster), substitutor::replace));
+
+        //build principal mapping
+        final List<Function<ClusterState, ClusterState>> stateTransforms = Collections.singletonList(cs -> cs.mapPrincipals(cluster.principals));
+
+        try {
+
+            File clusterWideACLs = null;
+
+            if( cluster.clusterLevelAccessPath != null ) {
+                clusterWideACLs = new File(cluster.clusterLevelAccessPath);
+            }
+
+            //todo: fix this
+            final var folders = cluster.domainFileFolders.stream().map(File::new).collect(Collectors.toList());
+            final Runner runner = new Runner(folders, clusterWideACLs , clientProps, stateTransforms);
+            runner.run();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+            System.exit(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
 
     public static void main(String[] args) {
         final int status = new CommandLine(new CLI()).execute(args);
